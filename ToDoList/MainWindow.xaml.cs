e51a1ec5307a6e465e70ef9fbc12173915b0ac95
Rob Garner <rgarner011235@gmail.com>
2024-04-02 19:03:21 -0600

    Added add button

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -28,5 +28,24 @@ namespace ToDoList
             _toDos = _repo.GetToDos();
             lbToDos.ItemsSource = _toDos;
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new ToDo object
+            ToDo toDo = new ToDo();
+            toDo.Title = txbTitle.Text;
+            toDo.Description = txbDesciption.Text;
+            toDo.Created = dtCreateDate.SelectedDate.Value;
+            toDo.Completed = cbCompleted.IsChecked.Value;
+
+            // Add the new ToDo object to database
+            toDo.Id = _repo.AddToDo(toDo);
+
+            // Add the new ToDo object to the list
+            _toDos.Add(toDo);
+
+            // Refresh the list
+            lbToDos.Items.Refresh();
+        }
     }
 }
